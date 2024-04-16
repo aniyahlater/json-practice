8e7cca03e5e31fa9a51b24c4f532c690eec0401d
Aniyah McWilliams <ybf3jw@virginia.edu>
2024-04-16 18:42:51 +0000

    prints all on one line; still needs fixing

--- labs/lab8/jsonscript.py
+++ labs/lab8/jsonscript.py
@@ -3,8 +3,12 @@
 import json
 with open('/workspace/json-practice/data/schacon.repos.json', 'r') as file:
     data = json.load(file)
+#going to only parse the first 5 entries 
+five = data[:5]
+for each in five:
 # pulling out the following fields: name, html_url, updated_at, and visibility
-#import requests
-#response=requests.get(data)
-#response.
-print(data)
+    with open("chacon.csv","a") as file:
+        csl = each['name'] +  ',' + each['html_url'] + ',' + each['updated_at'] + ',' + each['visibility']
+        file.write(csl)
+
+
